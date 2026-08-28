c424b7a20e1a06b243e0d3522e8e32046d17412e
IvBu <ivan.buntic@iws.uni-stuttgart.de>
2026-08-28 14:29:01 +0200

    Use variable host ip.

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 import uvicorn
+import os
 
 from api import create_app
+
+# use HOST=127.0.0.1 when running local tests
+#     HOST=0.0.0.0   when running in docker
+HOST_IP = os.environ["HOST"]
 
 
 app = create_app()
 
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host=HOST_IP, port=8000)
